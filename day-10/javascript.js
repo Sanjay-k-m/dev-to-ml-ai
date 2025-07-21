051cae97cf3822c5955d4d4cc255508e7190422a
sanjay-k-m <sanjay_k.m@outlook.com>
2025-07-21 09:44:24 +0530

    Day 10

---
+++ day-10/javascript.js
@@ -0,0 +1,10 @@
+let numb = 9 
+a = 5 
+b = 5
+c=2
+console.log(a===b)
+console.log(a ==b )
+console.log("Number is ",numb)
+console.log((a >b) || b<c)
+console.log((a >b) && b<c)
+console.log(a >b || b<c && a>b)
